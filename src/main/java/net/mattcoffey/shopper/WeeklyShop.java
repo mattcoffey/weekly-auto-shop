684db38d0e2ad42634d64cf0aeceb531e9d0b75d
Matt Coffey <matt.coffey.1983@gmail.com>
2015-11-22 22:46:47 +0000

    fixed workaround for broken empty trolly button and updated for changes in login screen
    Change-Id: I8ce78a69c76a0856eccf1daed4dfcaaefc31876b

--- src/main/java/net/mattcoffey/shopper/WeeklyShop.java
+++ src/main/java/net/mattcoffey/shopper/WeeklyShop.java
@@ -2,8 +2,10 @@ package net.mattcoffey.shopper;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -63,13 +65,20 @@ public class WeeklyShop {
      * (Workaround for the empty trolley button not functioning)
      */
     private void deleteAllItems() {
-        try {
-            for(WebElement element : driver.findElements(By.cssSelector("a[class='repressive delete']"))) {
-                element.click();
+        // The whole table is reloaded by Ajax call whenever an item changes so it is necessary to get the list of items every time one is removed
+        while(true) {
+            try {
+                List<WebElement> elements = driver.findElements(By.cssSelector("a[class='repressive delete']"));
+                if(elements.isEmpty()) {
+                    //Trolley empty
+                    return;
+                }
+                elements.iterator().next().click();
+                driver.get(driver.getCurrentUrl());
             }
-        }
-        catch(Exception e) {
-            //Already empty
+            catch (Exception e) {
+                logger.error("Error removing item from trolley:", e);
+            }
         }
     }
 
@@ -146,14 +155,14 @@ public class WeeklyShop {
     private void login() {
         logger.info("Logging in");
         driver.get("https://www.sainsburys.co.uk/webapp/wcs/stores/servlet/LogonView?catalogId=10122&langId=44&storeId=10151&krypto=gPjk3%2BzluQwvKX86bngreDpsD2PZbgo0k%2BK1QRr9ktf2LQnIbem8yOn9zO92uvtU47wuMM1Yd97g%0AwRxQ05ZbtOW1HNtDiSA68zyCVSw5uV9R9PLBIUgNcWdzfwJu76J6ROykfvF081ZfenNS8LhYE%2F01%0AbRVPm0jH87tBNFudtyc2ZyUb3GK0ST2YBKBCQsriu8fI7tQ9bMYDcTJGlUF2WVjvzFxupS6dU3wQ%0ANPDHhcCB%2FjyRsL3Tn9yV%2BlatV%2FmTIzOM8GyFfWO6NMxEEAs1KrGJ7kEXTA75%2BC5wAdALexl1DHvR%0AV6mpqjJL%2FP3upaHGkGuVtqtgGHdkntALwkqrb5ij2MBik0EBD%2FerGKB29gzlBNUnnRqAkoJ1%2Fhhj%0A53D4&ddkey=http:LogonView");
-        
-        WebElement username = driver.findElement(By.id("logonId"));
+        sleep(500L);
+        WebElement username = driver.findElement(By.name("logonId"));
         username.sendKeys(config.getUsername());
         
-        WebElement password = driver.findElement(By.id("logonPassword"));
+        WebElement password = driver.findElement(By.name("logonPassword"));
         password.sendKeys(config.getPassword());
         
-        WebElement login = driver.findElement(By.className("process"));
+        WebElement login = driver.findElement(By.cssSelector("input.button.process"));
         login.click();
     }
 
